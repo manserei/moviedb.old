b7436b1d0f1fed583f1c4dd8565398f24d6431cc
Hendrik Mans <hendrik@mans.de>
2013-03-26 16:37:39 +0100

    Complete first REST resource

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,36 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update_attributes(params[:movie])
+      redirect_to movie_path(@movie)
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(params[:movie])
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :action => 'new'
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
 end
