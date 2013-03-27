7332cc6f0640df858170ec78abf9dea5926ae27f
Hendrik Mans <hendrik@mans.de>
2013-03-27 17:22:50 +0100

    Another style of speccing

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe Movie do
   it "should only be valid with a title" do
     movie = Movie.new(:language => 'en', :length => 180, :year => 1976)
-    movie.should_not be_valid
+
+    movie.should_not be_valid     # bisherige syntax
+    expect(movie).to_not be_valid # neue syntax
 
     movie.title = 'Foo Bar 2 - The Return'
 
@@ -13,4 +15,19 @@ describe Movie do
     # neue syntax:
     expect(movie).to be_valid
   end
+
+  it "should only be valid with a title" do
+    movie = Movie.new(:language => 'en', :length => 180, :year => 1976)
+
+    expect { movie.title = "Star Wars Episode VII" }
+      .to change { movie.valid? }
+      .from(false).to(true)
+  end
+
+  # it "foo" do
+  #   expect {
+  #     Movie.create(:title => "Star Wars",
+  #       :length => 123, :year => 1977, :language => 'en')
+  #   }.to change { Movie.count }.by(1)
+  # end
 end
